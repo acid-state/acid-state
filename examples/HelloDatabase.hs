fb5f67a63421b9aa81e3056157fd471a4682bce1
Lemmih <lemmih@gmail.com>
2011-04-16 09:31:51 +0400

    Add another example.
    Ignore-this: 56ce38525a6494f9a36062d85c03ad51
    
    darcs-hash:20110416053151-26c35-401e471b7841080e31c6730b4d13503680847123

---
+++ examples/HelloDatabase.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE TypeFamilies, DeriveDataTypeable, TemplateHaskell #-}
+module Main (main) where
+
+import Data.Acid
+
+import Control.Monad.State                   ( get, put )
+import Control.Monad.Reader                  ( ask )
+import Control.Applicative                   ( (<$>) )
+import System.Environment                    ( getArgs )
+import qualified Data.Binary as Binary
+
+type Message = String
+data Database = Database [Message]
+
+instance Binary.Binary Database where
+    get = Database <$> Binary.get
+    put (Database msg) = Binary.put msg
+
+-- Transactions are defined to run in either the 'Update' monad
+-- or the 'Query' monad.                                                                                                                                    
+addMessage :: Message -> Update Database ()
+addMessage msg
+    = do Database messages <- get
+         put $ Database (msg:messages)
+
+viewMessages :: Int -> Query Database [Message]
+viewMessages limit
+    = do Database messages <- ask
+         return $ take limit messages
+
+-- This will define @ViewMessage@ and @AddMessage@ for us.
+$(makeAcidic ''Database ['addMessage, 'viewMessages])
+
+main :: IO ()
+main = do args <- getArgs
+          database <- openAcidStateFrom "myDatabase/" (Database ["Welcome to the acid-state database."])
+          if null args
+            then do messages <- query database (ViewMessages 10)
+                    putStrLn "Last 10 messages:"
+                    mapM_ putStrLn [ "  " ++ message | message <- messages ]
+            else do update database (AddMessage (unwords args))
+                    putStrLn "Your message has been added to the database."
