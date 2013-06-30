048a1aec2e5ae933dd064e32d7b0734cc4e5123a
Nikita Volkov <nikita.y.volkov@mail.ru>
2013-06-30 16:15:18 +0400

    Loading Benchmark

---
+++ benchmarks/loading/Benchmark/Model.hs
@@ -0,0 +1,15 @@
+module Benchmark.Model where
+
+import Benchmark.Prelude
+import qualified Data.Acid as Acid
+
+
+
+type Model = [[[Int]]]
+
+insert :: [[Int]] -> Acid.Update Model ()
+insert = modify . (:)
+
+Acid.makeAcidic ''Model ['insert]
+
+
