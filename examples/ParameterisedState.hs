daca8fbe04312fcba2b7f29205dfd74995966858
Adam Gundry <adam@well-typed.com>
2018-09-21 11:48:02 +0100

    Add example of state using parameterised type
    This has been a source of compile-time bugs in the past (#36, #52).

---
+++ examples/ParameterisedState.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module ParameterisedState (main) where
+
+import Control.Monad.State.Strict
+
+import Data.Acid
+import qualified Data.Map as Map
+import Data.SafeCopy (SafeCopy(..), deriveSafeCopy, base)
+import Data.Serialize
+import GHC.Generics
+
+data Entry k = Entry
+    { key :: !k
+    , val :: !Int
+    }
+    deriving (Eq, Ord, Generic)
+
+instance Serialize k => Serialize (Entry k)
+
+$(deriveSafeCopy 0 'base ''Entry)
+
+newtype Store k = Store { store :: Map.Map k (Entry k) }
+    deriving (Eq, Generic)
+
+instance (Ord k, Serialize k) => SafeCopy (Store k)
+instance (Ord k, Serialize k) => Serialize (Store k)
+
+insertStore
+    :: (Ord k, Serialize k)
+    => Entry k
+    -> Update (Store k) k
+insertStore item = do
+    modify $ \(Store s) -> Store $ Map.insert (key item) item s
+    return (key item)
+
+makeAcidic ''Store [ 'insertStore ]
+
+
+main :: IO ()
+main = do st <- openLocalState (Store Map.empty :: Store String)
+          k  <- update st (InsertStore (Entry "A" 42))
+          putStrLn k
